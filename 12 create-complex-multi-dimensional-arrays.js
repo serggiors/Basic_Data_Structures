6435084af1fd8d233cf8e0ee0fe8860e1bea34ab
Sergio Rios <serggio.rs@gmail.com>
2022-12-06 11:26:46 -0300

    homework basic_data_structures

---
+++ 12 create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,11 @@
+let myNestedArray = [
+  // Only change code below this line
+  [
+    ['nivel2'],['nivel2'],[
+      [
+        ['deepest'],
+        'deeper'],
+        'deep']
+        ],
+  // Only change code above this line
+];
